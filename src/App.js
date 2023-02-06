8b7b122f59a360b60a7e34028fdfde94dd5043b3
Kazimir <kazimirS46@yandex.ru>
2023-02-06 16:54:59 +0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import { List } from './components/List';
 
+const TEXT = {
+  h1: 'Список задач',
+};
+
 function App() {
   return (
     <div className='App'>
-      <h1>To do List</h1>
+      <h1>{TEXT.h1}</h1>
       <List />
     </div>
   );
--- src/components/FieldAddingTask.jsx
+++ src/components/FieldAddingTask.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const FIELDTEXT = {
+  holder: 'Добавь задачу здесь',
+  addButton: 'Добавить',
+};
+
 export const FieldAddingTask = ({ add }) => {
   const [inputValue, setInputValue] = React.useState('');
 
@@ -7,11 +12,16 @@ export const FieldAddingTask = ({ add }) => {
     setInputValue(evt.target.value);
   };
 
+  const clickAddButton = () => {
+    add(inputValue);
+    setInputValue('');
+  };
+
   return (
     <div className='container'>
       <div className='content'>
-        <input type='text' className='addTodosInput' placeholder='Добавь задачу здесь' value={inputValue} onChange={onChangeInput}></input>
-        <button onClick={() => add(inputValue)}>Добавить</button>
+        <input type='text' className='addTodosInput' placeholder={FIELDTEXT.holder} value={inputValue} onChange={onChangeInput}></input>
+        <button onClick={clickAddButton}>{FIELDTEXT.addButton}</button>
       </div>
     </div>
   );
